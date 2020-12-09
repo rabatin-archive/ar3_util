37a2dbb1d42ba26dcb901e9a821ac66485998ec8
Rabatin System Account <arthur@rabatin.com>
2020-12-08 23:10:55 -0500

    Repository Created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='ar3_util',
+  version='0.1',
+  packages=['tests', 'ar3_util'],
+  url='www.rabatin.com',
+  license='MIT',
+  author='Arthur Rabatin',
+  author_email='arthur@rabatin.com',
+  description='Collection of Utlities'
+)
